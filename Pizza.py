90c232c7a41d65bc996cca81c95beda2a57c837b
ianms17 <35043635+ianms17@users.noreply.github.com>
2020-06-18 19:11:52 -0500

    Added the backend code to enable the buttons to work, altered the pattern for how the menu would be generated, created a new window to house the menu

--- Pizza.py
+++ Pizza.py
@@ -4,22 +4,25 @@ import tkinter as tk
 '''
 Function: Create Customer
 '''
-def CreateCustomer():
-    pass
+def CreateCustomer(firstName, lastName, email, number):
+    customer = pizzapi.Customer(firstName.get(), lastName.get(), email.get(), number.get())
+    return customer
 
 
 '''
 Function: Create Address
 '''
-def CreateAddress():
-    pass
+def CreateAddress(street, city, state, zipcode):
+    address = pizzapi.Address(street.get(), city.get(), state.get(), zipcode.get())
+    return address
 
 
 '''
 Function: CreateCard
 '''
-def CreateCard():
-    pass
+def CreateCard(cardNum, expiration, cvv, zipcode):
+    card = pizzapi.PaymentObject(cardNum.get(), expiration.get(), cvv.get(), zipcode.get())
+    return card
 
 
 '''
@@ -32,15 +35,19 @@ def CreateOrder():
 '''
 Function: Submit
 '''
-def Submit():
-    print("Submitted")
+def SubmitOrder(customer, address, card):
+    order = pizzapi.Order(address.closest_store(), customer, address)
+    order.pay_with(card)
+
 
 
 '''
 Function: Get Menu Items
 '''
-def GetMenuItems():
-    return 'default'
+def GetMenuItems(address):
+    store = address.closest_store()
+    menu = store.getMenu()
+    return menu.search(name='Pizza')
 
 
 '''
@@ -56,6 +63,7 @@ numberLabel = tk.Label(text='Phone Number')
 streetLabel = tk.Label(text='Street')
 cityLabel = tk.Label(text='City')
 stateLabel = tk.Label(text='State')
+zipLabel = tk.Label(text='Zip Code')
 creditLabel = tk.Label(text='Credit Card Number')
 expirationLabel = tk.Label(text='Expiration Date (MMYY)')
 cvLabel = tk.Label(text='Security Code')
@@ -68,6 +76,7 @@ numberEntry = tk.Entry()
 streetEntry = tk.Entry()
 cityEntry = tk.Entry()
 stateEntry = tk.Entry()
+zipEntry = tk.Entry()
 creditEntry = tk.Entry()
 expirationEntry = tk.Entry()
 cvEntry = tk.Entry()
@@ -87,6 +96,8 @@ cityLabel.pack()
 cityEntry.pack()
 stateLabel.pack()
 stateEntry.pack()
+zipLabel.pack()
+zipEntry.pack()
 creditLabel.pack()
 creditEntry.pack()
 expirationLabel.pack()
@@ -94,17 +105,28 @@ expirationEntry.pack()
 cvLabel.pack()
 cvEntry.pack()
 
-# add dropdown menu for the menu items
-menuLabel = tk.Label(text='Menu Options')
-menuItems = GetMenuItems()
-default = 'Empty'
-menuDropdown = tk.OptionMenu(window, default, *menuItems)
-menuLabel.pack()
-menuDropdown.pack()
+newCustomer = CreateCustomer(fNameEntry, lNameEntry, emailEntry, numberEntry)
+newAddress = CreateAddress(streetEntry, cityEntry, stateEntry, zipEntry)
+newCard = CreateCard(creditEntry, expirationEntry, cvEntry, zipEntry)
+
+
+'''
+Function: GetMenu
+'''
+def CreateMenu(address):
+    menuWindow = tk.Tk()
+    textbox = tk.Text(menuWindow, height='40', width='40')
+    textbox.pack()
+    textbox.insert(tk.END, GetMenuItems(newAddress))
+
+
+# add a view menu button
+viewMenuButton = tk.Button(text='View Menu')
+viewMenuButton.bind("<Button-1>", CreateMenu(newAddress))
 
 # add submit button
 submitButton = tk.Button(text='Submit')
-submitButton.bind("<Button-1>", Submit())
+submitButton.bind("<Button-1>", SubmitOrder())
 submitButton.pack()
 
 
